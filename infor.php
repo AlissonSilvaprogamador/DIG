46057baf801607f7ae0af053fbf3e88dbe5c0ec3
Alissonsilvaprogramador <digtec21@gmail.com>
2022-08-31 15:50:04 -0300

    Initial commit

---
+++ infor.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+    $tNome = $_GET["tNome"]
+    $tSenha = $_GET["senha"]
+    $tMail = $_GET["e-mail"]
+    $tSexo = $_GET["sexo"]
+    $tData = $_GET["data"]
+    $tRua = $_GET["rua"]
+    $tNumero = $_GET["numero"]
+    $tEst = $_GET["estado"]
+    $tCid = $_GET["cidade"]
+    $tMsg = $_GET["msg"]
+        
+    ?>
+</div>
+</body>
+</html>
+ 
